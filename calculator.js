c150ee244cc3b70905d68cb20b1dbab04a02bd76
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-19 13:47:23 -0400

    Catch basic post request

--- calculator.js
+++ calculator.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", (req, res) => {
+    res.send("Thanks");
+});
+
 app.listen(port, () => {
     console.log("Servicer is listening on port " + port);
 });
