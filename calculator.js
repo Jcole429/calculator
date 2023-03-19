1c46149d94e30ce49c740292cb4321a28c94e8b5
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-19 14:02:06 -0400

    Parse and add num1 and num2

--- calculator.js
+++ calculator.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    res.send("Thanks");
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The result of the calculation is " + result);
 });
 
 app.listen(port, () => {
